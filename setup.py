f7e0f01453de5598168845abf43d3b923e23994a
vigneswaran <134774303+megalosVigneswaran@users.noreply.github.com>
2023-05-31 12:29:10 +0530

    Update setup.py
    update

--- setup.py
+++ setup.py
@@ -3,24 +3,33 @@ from setuptools import setup, find_packages
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
+  'Intended Audience :: End Users/Desktop',
+  'License :: Freeware',
+  'Operating System :: MacOS :: MacOS X',
+  'Topic :: Education',
+  'Operating System :: POSIX'
   'Operating System :: Microsoft :: Windows :: Windows 10',
   'Operating System :: Microsoft :: Windows :: Windows 8.1',
   'Operating System :: Microsoft :: Windows :: Windows 8',
   'License :: OSI Approved :: MIT License',
-  'Programming Language :: Python :: 3'
+  'Programming Language :: Python',
+  'Programming Language :: Python :: 3.8'
 ]
 
 setup(
   name='vtrans',
-  version='1.0.0',
-  description='This is a self-updating translator powered by Google Translate for free and unlimited usage.',
-  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='https://github.com/megalosVigneswaran/vtrans/tree/main/vtrans',  
+  version='3.0.0',
+  description='This is a self-updating translator for free and unlimited usage with upto 130 langauges',
+  long_description=open('README.rst',"r",encoding="utf-8").read(),
+  url='https://github.com/megalosVigneswaran/vtrans/',  
   author='S.Vigneswaran',
   author_email='t896242@gmail.com',
   license='MIT',
   classifiers=classifiers,
   keywords=['translator', 'googletranslate', 'self-updating', 'googletrans'],
   packages=find_packages(),
+  package_data={
+        'vtrans': ['config.txt'],
+    },
   install_requires=['googletrans==3.1.0a0', 'pandas', 'requests', 'beautifulsoup4', 'astor'] 
 )
